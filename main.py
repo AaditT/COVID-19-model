0add776bfb5d0478e940dea07709a6a3bf289c13
Aadit Trivedi <aadit.trivedi03@gmail.com>
2020-03-14 14:26:17 -0700

    Update and rename thailand_model.py to main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import plotly.graph_objects as go
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
+from sklearn import linear_model
 
 regions = []
-
 dict = {}
 
 df = pd.read_csv('data/time_series_19-covid-Confirmed.csv')
 col = df.columns.tolist()
 # print(col)
+
 with open('data/time_series_19-covid-Confirmed.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     for row in csvReader:
@@ -24,7 +24,6 @@ thailand_dict.pop("Province/State", None)
 thailand_dict.pop("Country/Region", None)
 thailand_dict.pop("Lat", None)
 thailand_dict.pop("Long", None)
-
 dates = []
 cases = []
 
@@ -34,20 +33,15 @@ for key in thailand_dict:
     cases.append(thailand_dict[key])
 
 numbers = []
+
 for i in range(len(dates)):
     numbers.append(i)
-
 
 with open('test.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',')
     spamwriter.writerow(['x','cases'])
     for i in range(len(numbers)):
         spamwriter.writerow([numbers[i],cases[i]])
-
-
-import pandas as pd
-import numpy as np
-from sklearn import linear_model
 
 df = pd.read_csv("test.csv")
 reg = linear_model.LogisticRegression()
@@ -64,12 +58,9 @@ with open('model.csv', 'w', newline='') as csvfile:
     for i in range(len(numbers)):
         spamwriter.writerow([numbers[i],model(numbers[i])])
 
-
-
-
-
 model_cases = []
 model_cases_ = []
+
 for i in range(len(numbers)):
     model_cases.append(model(numbers[i]))
 
@@ -77,20 +68,7 @@ for val in model_cases:
     val = list(val)[0]
     model_cases_.append(val)
 
-import plotly.graph_objects as go
-
 fig = go.Figure()
-
-fig.add_trace(
-    go.Bar(
-        x=numbers,
-        y=cases,
-    ))
-
-fig.add_trace(
-    go.Scatter(
-        x=numbers,
-        y=model_cases_,
-    ))
-
+fig.add_trace(go.Bar(x=numbers,y=cases))
+fig.add_trace(go.Scatter(x=numbers,y=model_cases_))
 fig.show()
